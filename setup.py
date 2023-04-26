9e668839c7ca5bc26c758c7e5769e104cd3fd85f
ChiaJung-Yeh <robert1328.tem06@nctu.edu.tw>
2023-04-26 23:28:59 +0800

    Update

--- setup.py
+++ setup.py
@@ -3,20 +3,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='nycu_tdx_py',
-    version='0.1',
+    version='0.1.1',
     license='NYCU',
     author="Robert Yeh",
     author_email='robert1328.mg10@nycu.edu.tw',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/gmyrianthous/example-publish-pypi',
+    url='https://github.com/ChiaJung-Yeh/nycu_tdx_py',
     keywords='example project',
     install_requires=[
           'pandas',
           'geopandas',
           'numpy',
           'shapely',
-          'json',
           'requests'
       ],
 
