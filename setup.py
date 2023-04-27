05beb131837e9e73a66d8e1111602c283d8d2f65
ChiaJung-Yeh <robert1328.tem06@nctu.edu.tw>
2023-04-27 10:58:40 +0800

    README update

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from setuptools import setup, find_packages
 setup(
     name='nycu_tdx_py',
     version='0.1.11',
-    license='NYCU',
+    description='Python library for connecting to TDX'
+    license='MIT',
     author="Robert Yeh",
     author_email='robert1328.mg10@nycu.edu.tw',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/ChiaJung-Yeh/nycu_tdx_py',
-    keywords='example project',
+    keywords='tdx transport',
     install_requires=[
           'pandas',
           'geopandas',
